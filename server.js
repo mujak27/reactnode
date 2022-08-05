c10acda5b821c789c6b1591d4061c0fbe0d8139e
mujak27 <94689468+mujak27@users.noreply.github.com>
2022-08-05 14:00:20 +0700

    port env

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
-// 👇️ "/home/john/Desktop/javascript"
+
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.resolve(__dirname, 'build/')));
 
-app.get('*', (req, res)=>{
+app.get('/', (req, res)=>{
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 })
 
-app.listen(5000, ()=>{
+app.listen(port, ()=>{
   console.log('server started')
 })
